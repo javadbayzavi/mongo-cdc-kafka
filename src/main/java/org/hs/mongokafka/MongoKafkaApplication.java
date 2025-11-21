e71e104c392405c4a1761c17c061faf25b5920a4
Javad Bayzavi <javadbayzavi@gmail.com>
2025-11-21 14:36:53 +0100

    App root cleanup

--- src/main/java/org/hs/mongokafka/MongoKafkaApplication.java
+++ src/main/java/org/hs/mongokafka/MongoKafkaApplication.java
@@ -1,24 +1,12 @@
 package org.hs.mongokafka;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class MongoKafkaApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(MongoKafkaApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner run() {
-        return args -> {
-            MongoListener listener;
-            listener.listen();
-        };
     }
 }
